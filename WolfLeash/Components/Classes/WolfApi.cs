64ed77b9c4926b04e8daa14f2975a345cde3f5b3
Sebastian Sommer <mail@sommer-sebastian.de>
2026-01-24 17:11:09 +0100

    Added an auto shutdown on 3 failed connection retrys when accessing the SSE. Resets after 10sec, retrys occure in 5sec interval.

--- WolfLeash/Components/Classes/WolfApi.cs
+++ WolfLeash/Components/Classes/WolfApi.cs
@@ -4,7 +4,17 @@ namespace WolfLeash.Components.Classes;
 
 public class Api : WolfApi
 {
-    public Api(ILogger<WolfApi> logger, IConfiguration configuration) : base(logger, configuration) { }
+    private readonly ILogger<WolfApi> _logger;
+    private readonly IHostApplicationLifetime _lifeCycleService;
+    private int _connectionErrorCount = 0;
+    private DateTime _lastConnectionErrorTime = DateTime.MinValue;
+    
+    public Api(ILogger<WolfApi> logger, IConfiguration configuration, IHostApplicationLifetime lifetime) : 
+        base(logger, configuration)
+    {
+        _lifeCycleService = lifetime;
+        _logger = logger;
+    }
 
     private static Task Raise<TSource, TEventArgs>(Func<TSource, TEventArgs, Task>? handlers, TSource source, TEventArgs args)
     {
@@ -17,7 +27,21 @@ public class Api : WolfApi
 
         return Task.CompletedTask;
     }
-    
+
+    protected override Task OnSseConnectionLostEvent(bool isFatal)
+    {
+        if (DateTime.Now - _lastConnectionErrorTime > TimeSpan.FromSeconds(10))
+        {
+            _connectionErrorCount = 0;
+            _lastConnectionErrorTime = DateTime.Now;
+        }
+        if (_connectionErrorCount++ <= 3) return base.OnSseConnectionLostEvent(isFatal);
+        _logger.LogError("Can't connect to Socket, shutting down.");
+        _lifeCycleService.StopApplication();
+
+        return base.OnSseConnectionLostEvent(isFatal);
+    }
+
     protected override async Task OnPairSignalEvent(string data)
     {
         await Raise(PairRequestEvent, this, data);
